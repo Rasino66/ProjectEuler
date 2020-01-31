bf2c8b5e6209ecd93cccdd5bee31400dbccf86d0
rasino66 <a.rasino66@gmail.com>
2020-01-31 16:34:51 +0900

    complete 003

--- Problem003/Probrem003.go
+++ Problem003/Probrem003.go
@@ -2,18 +2,37 @@ package main
 
 import "fmt"
 
-func main(){
-  var fib_a int = 1
-  var fib_b int = 2
-  var fib_tmp int = 0
-  var anser int = 2
-  for anser <= 4000000 {
-    fib_tmp = fib_a + fib_b
-    if fib_tmp % 2 == 0 {
-      anser = anser + fib_tmp
-    }
-    fib_a = fib_b
-    fib_b = fib_tmp
+func factorization(jude_number int) []int{
+  var anser []int
+  var i int = 1
+  prime := []int{2}
+
+  if jude_number % 2 == 0 {
+    anser = append(anser, 2)
   }
-  fmt.Println(anser)
+  for jude_number != 1 {
+    FOR_LABEL:
+    i = i + 2
+    //素数判定
+    for _, prime_num := range prime{
+      if i % prime_num == 0 {
+        goto FOR_LABEL
+      }
+    }
+    //素因数の判定式
+    prime = append(prime, i)
+    if jude_number % i == 0 {
+      anser = append(anser, i)
+      jude_number = jude_number / i
+      fmt.Println("%d",jude_number)
+    }
+  }
+  return anser
+}
+
+func main(){
+  var JUDE_NUMBER int = 600851475143
+  tmp := factorization(JUDE_NUMBER)
+  fmt.Println(tmp)
+
 }
